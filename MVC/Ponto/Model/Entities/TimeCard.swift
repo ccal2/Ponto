7b4108ab83ec2d3ff531d999f0e26245f7f9759d
Carolina Lopes <ccal2@cin.ufpe.br>
2022-04-11 22:46:01 -0300

    Add basic entities - TimeCard and Break (#1)
    * Add Break entity
    
    * Add time card entity
    
    * Turn DateProvider into singleton

---
+++ MVC/Ponto/Model/Entities/TimeCard.swift
@@ -0,0 +1,132 @@
+//
+//  TimeCard.swift
+//  Ponto-MVC
+//
+//  Created by Carolina Cruz Agra Lopes on 03/20/22.
+//
+
+import Foundation
+
+class TimeCard {
+
+    // MARK: - Properties
+
+    private(set) var startDate: Date
+    private(set) var endDate: Date? = nil {
+        didSet {
+            // It's not possible to modify the endDate while on a break
+            state = (endDate == nil) ? .ongoing : .finished
+        }
+    }
+    private(set) var breaks: [Break] = []
+
+    private(set) var state: State
+
+    private var currentDateProvider: CurrentDateProvider
+
+    var duration: TimeInterval {
+        let referenceDate = endDate ?? currentDateProvider.currentDate()
+        let breakTime = breaks.map({ $0.duration }).reduce(0, +)
+
+        return referenceDate.timeIntervalSince(startDate) - breakTime
+    }
+
+    // MARK: - Initializer
+
+    init(start: Date, currentDateProvider: CurrentDateProvider = DateProvider.sharedInstance) {
+        self.startDate = start
+        self.currentDateProvider = currentDateProvider
+        self.state = .ongoing
+    }
+
+    // MARK: - Internal Methods
+
+    func startBreak() throws {
+        switch state {
+        case .ongoing:
+            let newBreak = Break(start: currentDateProvider.currentDate(),
+                                 currentDateProvider: currentDateProvider)
+            breaks.append(newBreak)
+            state = .onABreak
+        case .onABreak:
+            throw TimeCardError.alreadyOnABreak
+        case .finished:
+            throw TimeCardError.alreadyFinished
+        }
+    }
+
+    func currentBreak() throws -> Break? {
+        let openBreaks = breaks.filter { aBreak in aBreak.endDate == nil }
+
+        guard openBreaks.count <= 1 else {
+            throw TimeCardError.multipleUnfinishedBreaks
+        }
+
+        return openBreaks.last
+    }
+
+    func finishBreak() throws {
+        guard let currentBreak = try currentBreak() else {
+            assert(state != .onABreak)
+            throw TimeCardError.notOnABreak
+        }
+        assert(state == .onABreak)
+
+        try currentBreak.finish()
+        state = .ongoing
+    }
+
+    func finish() throws {
+        switch state {
+        case .ongoing:
+            endDate = currentDateProvider.currentDate()
+        case .onABreak:
+            throw TimeCardError.onABreak
+        case .finished:
+            assert(endDate != nil)
+            throw TimeCardError.alreadyFinished
+        }
+    }
+
+}
+
+// MARK: - State
+
+extension TimeCard {
+
+    enum State {
+        // Cases
+        case ongoing
+        case onABreak
+        case finished
+    }
+
+}
+
+// MARK: - Errors
+
+enum TimeCardError: LocalizedError {
+    // Cases
+    case alreadyFinished
+    case alreadyOnABreak
+    case multipleUnfinishedBreaks
+    case notOnABreak
+    case onABreak
+
+    // Description
+    var errorDescription: String? {
+        switch self {
+        case .alreadyFinished:
+            return NSLocalizedString("The time card has already finished", comment: "Error description")
+        case .alreadyOnABreak:
+            return NSLocalizedString("The time card is already on a break", comment: "Error description")
+        case .multipleUnfinishedBreaks:
+            return NSLocalizedString("The time card has multiple unfinished breaks", comment: "Error description")
+        case .notOnABreak:
+            return NSLocalizedString("The time card is not currently on a break", comment: "Error description")
+        case .onABreak:
+            return NSLocalizedString("The time card is currently on a break", comment: "Error description")
+        }
+    }
+
+}
---
+++ MVC/PontoTests/Model/BreakTests.swift
@@ -0,0 +1,94 @@
+//
+//  BreakTests.swift
+//  Ponto-MVCTests
+//
+//  Created by Carolina Cruz Agra Lopes on 02/13/22.
+//
+
+import XCTest
+@testable import Ponto_MVC
+
+class BreakTests: XCTestCase {
+
+    // MARK: - Properties
+
+    var mockDateProvider: MockDateProvider!
+
+    // MARK: - Set up & Tear down
+
+    override func setUpWithError() throws {
+        mockDateProvider = MockDateProvider()
+        try mockDateProvider.updateDate(to: "02/01/97 15:00")
+    }
+
+    override func tearDownWithError() throws {
+        mockDateProvider = nil
+    }
+
+    // MARK: - Tests
+
+    // MARK: duration
+
+    func test_duration_beforeFinishing() throws {
+        // Arrange
+        let workBreak = Break(start: mockDateProvider.currentDate(),
+                              currentDateProvider: mockDateProvider)
+
+        // Act
+        try mockDateProvider.updateDate(to: "02/01/97 15:15")
+
+        // Assert
+        XCTAssertEqual(workBreak.duration, 15 * Constants.minutesToSeconds)
+    }
+
+    func test_duration_afterFinishing() throws {
+        // Arrange
+        let workBreak = Break(start: mockDateProvider.currentDate(),
+                              currentDateProvider: mockDateProvider)
+        try mockDateProvider.updateDate(to: "02/01/97 15:15")
+        try workBreak.finish()
+
+        // Act
+        try mockDateProvider.updateDate(to: "02/01/97 15:20")
+
+        // Assert
+        XCTAssertEqual(workBreak.duration, 15 * Constants.minutesToSeconds)
+    }
+
+    // MARK: finish
+
+    func test_finish_updatesEndDate() throws {
+        // Arrange
+        let workBreak = Break(start: mockDateProvider.currentDate(),
+                              currentDateProvider: mockDateProvider)
+
+        // Act
+        try mockDateProvider.updateDate(to: "02/01/97 15:15")
+        try workBreak.finish()
+        try mockDateProvider.updateDate(to: "02/01/97 15:20")
+
+        // Assert
+        XCTAssertEqual(workBreak.endDate, mockDateProvider.dateFormatter.date(from: "02/01/97 15:15"))
+    }
+
+    func test_finish_afterAlreadyFinishing_throwsError() throws {
+        // Arrange
+        let workBreak = Break(start: mockDateProvider.currentDate(),
+                              currentDateProvider: mockDateProvider)
+        try mockDateProvider.updateDate(to: "02/01/97 15:15")
+        try workBreak.finish()
+
+        // Act
+        try mockDateProvider.updateDate(to: "02/01/97 15:20")
+        do {
+            try workBreak.finish()
+            XCTFail("Trying to finish a break that has already been finished before should fail with error `BreakError.alreadyFinished`")
+        } catch BreakError.alreadyFinished {
+            // Assert
+            // OK - expected error
+        } catch {
+            XCTFail("Expected `BreakError.alreadyFinished` error, but got: \(error) (\(error.localizedDescription))")
+        }
+    }
+
+}
