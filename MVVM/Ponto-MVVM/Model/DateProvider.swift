5bb5f1572a7ebf0553f569ee0b984f265b57fa2b
Carolina Lopes <ccal2@cin.ufpe.br>
2022-07-14 20:17:10 -0300

    Create MVVM project

---
+++ MVVM/Ponto-MVVM/Model/DateProvider.swift
@@ -0,0 +1,24 @@
+//
+//  DateProvider.swift
+//  Ponto-MVVM
+//
+//  Created by Carolina Cruz Agra Lopes on 02/13/22.
+//
+
+import Foundation
+
+protocol CurrentDateProvider {
+    func currentDate() -> Date
+}
+
+class DateProvider: CurrentDateProvider {
+
+    static let sharedInstance = DateProvider()
+
+    private init() { }
+
+    func currentDate() -> Date {
+        Date()
+    }
+
+}
