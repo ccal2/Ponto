f97b748af1002c94e38f7036f5e4e4d7c74e4cdb
Carolina Lopes <ccal2@cin.ufpe.br>
2022-07-20 20:34:48 -0300

    Create MVVM project

---
+++ Ponto-MVVM/Ponto-MVVM/App/Ponto_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ponto_MVVMApp.swift
+//  Ponto-MVVM
+//
+//  Created by Carolina Cruz Agra Lopes on 20/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Ponto_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
