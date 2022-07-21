65563e9ec3989742e62247639751b0f524f522f3
Carolina Lopes <ccal2@cin.ufpe.br>
2022-07-21 20:03:55 -0300

    Add View and ViewModel folders

--- Ponto-MVVM/Ponto-MVVM/App/Ponto_MVVMApp.swift
+++ Ponto-MVVM/Ponto-MVVM/App/Ponto_MVVMApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct Ponto_MVVMApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Text("Hello, world!")
+                .padding()
         }
     }
 }
