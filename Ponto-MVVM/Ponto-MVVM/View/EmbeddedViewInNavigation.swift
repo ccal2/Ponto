f35d520cc73abc9aa948674f22509329a1c6d2b2
Carolina Lopes <ccal2@cin.ufpe.br>
2022-09-01 00:10:04 -0300

    Embed views in a tabView

---
+++ Ponto-MVVM/Ponto-MVVM/View/EmbeddedViewInNavigation.swift
@@ -0,0 +1,20 @@
+//
+//  EmbeddedViewInNavigation.swift
+//  Ponto-MVVM
+//
+//  Created by Carolina Cruz Agra Lopes on 31/08/22.
+//
+
+import SwiftUI
+
+struct EmbeddedViewInNavigation: View {
+
+    let embeddedView: AnyView
+
+    var body: some View {
+        NavigationView {
+            embeddedView
+        }
+    }
+
+}
